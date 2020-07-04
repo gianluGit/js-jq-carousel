6294b1fbfb841e11e6d97e87790d703e8a4f18ef
Gianluca <gianlucarecchia1993@gmail.com>
2020-07-04 12:34:29 +0200

    slider- aggunta keydown e funzioni

--- main.js
+++ main.js
@@ -3,29 +3,70 @@ $(document).ready(function(){
   var activeNext = $('.next');
   var activePrev = $('.prev');
 
-  // ora aggiungo l'evento click
+  // ora aggiungo l'evento click per scorrere in avanti lo slider
   activeNext.click(function() {
-    var checkClassFirst = $('.images img.first').hasClass('active'); //torna true o false
-    var checkClassLast = $('.images img.last').hasClass('active');
-    var checkClassLastPrev = $('img.last').prev().hasClass('active');
-    $('img.first').removeClass('active');
-    //console.log($('.images img.active'));
-    if (checkClassFirst == true) {
-      $('img.first').next().addClass('active');
-    } else if (checkClassFirst == false && checkClassLastPrev == false && checkClassLast == false) {
-      $('img.first').next().removeClass('active');
-      $('img.last').prev().addClass('active');
-    } else if (checkClassLastPrev == true && checkClassLast == false) {
-      $('img.last').prev().removeClass('active');
-      $('img.last').addClass('active');
-    } else {
-      $('img.last').removeClass('active');
-      $('img.first').addClass('active');
-    }
-
+    nextArrow();
   });
+
+  //scorrimento all'indietro
   activePrev.click(function() {
-
+    prevArrow();
   });
 
+  // eventKeyCode
+  $(document).keydown(function() {
+    if (event.which == 39) {
+      nextArrow();
+    } else if (event.which == 37) {
+      prevArrow();
+    }
+    console.log(event.which);
+  });
 });
+
+
+// FUNCTION
+function nextArrow() {
+  var checkImgLast = $('.images img.last').hasClass('active'); //torna true o false
+  var activeImg = $('.images img.active');
+  activeImg.removeClass('active');
+  activeImg.next().addClass('active');
+  //console.log(activeImg);
+  if (checkImgLast == true) {
+    activeImg.removeClass('active');
+    $('.images img.first').addClass('active');
+  }
+  // scorrimrnto nav in avanti
+  var checkILast = $('.nav i.last').hasClass('active'); //torna true o false
+  var activeCircle = $('.nav i.active');
+  activeCircle.removeClass('active');
+  activeCircle.next().addClass('active');
+  //console.log(activeCircle);
+  if (checkILast == true) {
+    activeCircle.removeClass('active');
+    $('.nav i.first').addClass('active');
+  }
+}
+
+
+function prevArrow() {
+  var checkImgFirst = $('.images img.first').hasClass('active'); //torna true o false
+  var activeImg = $('.images img.active');
+  activeImg.removeClass('active');
+  activeImg.prev().addClass('active');
+
+  if (checkImgFirst == true) {
+    activeImg.removeClass('active');
+    $('.images img.last').addClass('active');
+  }
+  //scorrimento nav indietro
+  var checkIFirst = $('.nav i.first').hasClass('active'); //torna true o false
+  var activeCircle = $('.nav i.active');
+  activeCircle.removeClass('active');
+  activeCircle.prev().addClass('active');
+
+  if (checkIFirst == true) {
+    activeCircle.removeClass('active');
+    $('.nav i.last').addClass('active');
+  }
+}
